cc30a645ed5ee35d0d5b4bb79a4c954ac9431d34
Administrator <admin@example.com>
2025-12-06 14:23:40 +0800

    删除所有项目文件：清理工作区

--- src/core/algorithms/geometry/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 几何计算模块导出
- */
-
-export { Vector3 } from './Vector3';
-export { Plane } from './Plane';
-export { Matrix4 } from './Matrix4';
-export { TriangleIntersection, planeTriangleIntersection } from './TriangleIntersection';
